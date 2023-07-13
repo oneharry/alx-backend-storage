1320666ae6a71935badaf9f295be65b16a6ad2d2
oneharry <harryofficialchidi@yahoo.com>
2023-07-13 17:31:47 +0000

    calculate and store avg score using procedure:

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- creates a stored procedure that computes and stores students avg score
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	    DECLARE avgerage INT;
+	    SELECT AVG(score) INTO avgerage
+	    FROM corrections
+	    WHERE user_id = user_id;
+
+	    UPDATE users
+	    SET average_score = avgerage
+	    WHERE id = user_id;
+END $$
+
+DELIMITER ;
