50536d897c290f321a8831bb0615248e48028d08
oneharry <harryofficialchidi@yahoo.com>
2023-07-13 19:30:20 +0000

    added delimiter

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,10 +1,15 @@
 -- create a function that divides and returns  first and second number
 -- or returns 0 if the second number is equals 0
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT DETERMINISTIC
+DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
-	IF b == 0 THEN
-		RETURN 0
+	IF b = 0 THEN
+		RETURN 0;
 	ELSE 
-		RETURN (a / b)
+		RETURN (a / b);
 	END IF;
-END;
+END $$
+
+DELIMITER ;
