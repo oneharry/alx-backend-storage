f29a4b7a966a045d2700604d62257ca87db5314b
oneharry <harryofficialchidi@yahoo.com>
2023-07-13 18:10:30 +0000

    index names table

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,2 @@
--- index he first letter of name on names table
-CREATE INDEX index_name ON names(name(1));
+-- create index on the first letter of name on names table
+CREATE INDEX index_name ON names (name(1));
