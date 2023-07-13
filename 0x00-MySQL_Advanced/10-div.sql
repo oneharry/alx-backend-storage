0039f5843de684684a44a978ca7955b7acd22995
oneharry <harryofficialchidi@yahoo.com>
2023-07-13 19:15:43 +0000

    task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,10 @@
+-- create a function that divides and returns  first and second number
+-- or returns 0 if the second number is equals 0
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT DETERMINISTIC
+BEGIN
+	IF b = 0 THEN
+		RETURN 0
+	ELSE 
+		RETURN a / b
+	END IF;
+END;
