2e96deee6200828a96532f2a8e9830ff0aff4ae8
oneharry <harryofficialchidi@yahoo.com>
2023-07-13 17:03:16 +0000

    implemented email reset

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Create a trigger that resets email when a valid email is added
+DELIMITER $$
+
+CREATE TRIGGER reset_email_trigger
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email <> OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END $$
+
+DELIMITER ;
